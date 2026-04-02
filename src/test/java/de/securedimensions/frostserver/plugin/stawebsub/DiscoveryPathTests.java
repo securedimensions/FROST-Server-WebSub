19434fd7dd98eec8587d2882e1605ccf784f86d6
Andreas Matheus <andreas.matheus@secure-dimensions.de>
2026-04-02 10:10:34 +0200

    tests updated to final version of the STA-WebSub standard (OGC #24-032r1)

---
+++ src/test/java/de/securedimensions/frostserver/plugin/stawebsub/DiscoveryPathTests.java
@@ -0,0 +1,296 @@
+/*
+ * Copyright (C) 2024 Secure Dimensions GmbH, D-81377
+ * Munich, Germany.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.securedimensions.frostserver.plugin.stawebsub.test;
+
+import static de.securedimensions.frostserver.plugin.stawebsub.PluginWebSub.TAG_ERROR_TOPIC_NOT_ALLOWED;
+
+import de.fraunhofer.iosb.ilt.frostclient.SensorThingsService;
+import de.fraunhofer.iosb.ilt.frostclient.models.SensorThingsPlus;
+import de.fraunhofer.iosb.ilt.frostclient.models.SensorThingsV11MultiDatastream;
+import de.fraunhofer.iosb.ilt.frostclient.models.SensorThingsV11Sensing;
+import de.fraunhofer.iosb.ilt.statests.AbstractTestClass;
+import de.fraunhofer.iosb.ilt.statests.ServerVersion;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.*;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Tests for the W3C Discovery for URLs that have no query segment (no ODATA part)
+ * e.g. http://localhost:8080/FROST-Server/v1.1/Observations
+ *
+ * @author Andreas Matheus
+ */
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public abstract class DiscoveryPathTests extends AbstractTestClass {
+
+    /**
+     * The logger for this class.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(DiscoveryPathTests.class);
+    private static final long serialVersionUID = 1639739965;
+    private static final Map<String, String> SERVER_PROPERTIES = new LinkedHashMap<>();
+
+    static {
+
+        SERVER_PROPERTIES.put("persistence.idGenerationMode", "ServerAndClientGenerated");
+        SERVER_PROPERTIES.put("plugins.coreModel.enable", "true");
+        SERVER_PROPERTIES.put("plugins.coreModel.idType", "LONG");
+        SERVER_PROPERTIES.put("mqtt.Enabled", "false");
+        SERVER_PROPERTIES.put("mqtt.enabled", "false");
+        SERVER_PROPERTIES.put("plugins.plugins", "de.securedimensions.frostserver.plugin.stawebsub.PluginWebSub");
+        SERVER_PROPERTIES.put("plugins.stawebsub.enable", "true");
+        SERVER_PROPERTIES.put("plugins.stawebsub.hubUrl", "https://websub-hub.citiobs.secd.eu/api/subscriptions");
+        SERVER_PROPERTIES.put("plugins.stawebsub.helpUrl", "https://github.com/securedimensions/FROST-Server-WebSub/help.html");
+    }
+
+    private static final String[] rootTopicsOthers = {"Foo", "/", "", " ", "#"};
+    private static final String[] rootTopicSingle = {"FeaturesOfInterest(1)"};
+    private static Map<String, String[]> TEST_DATA = new HashMap<>();
+
+    static {
+        // Other test entities
+        TEST_DATA.put("Foo", new String[]{"404", null, null});
+        TEST_DATA.put("/", new String[]{"200", null, null});
+        TEST_DATA.put("", new String[]{"200", null, null});
+        TEST_DATA.put(" ", new String[]{"200", null, null});
+        TEST_DATA.put("#", new String[]{"200", null, null});
+    }
+
+    protected static SensorThingsService serviceSTAplus;
+
+    public DiscoveryPathTests(ServerVersion version) {
+        super(version, SERVER_PROPERTIES);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        LOGGER.info("Tearing down.");
+    }
+
+    @Override
+    protected void setUpVersion() {
+        LOGGER.info("Setting up for version {}.", version.urlPart);
+        serviceSTAplus = new SensorThingsService(
+                new SensorThingsV11Sensing(),
+                new SensorThingsV11MultiDatastream(),
+                new SensorThingsPlus());
+        try {
+            serviceSTAplus.setBaseUrl(new URL(serverSettings.getServiceUrl(version)));
+            serviceSTAplus.init();
+            createFeatureOfInterest();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*
+     * Success: self Link is not returned or equal to request URL
+     */
+    @Test
+    public void testDiscoveryByPath() throws IOException {
+        for (String entity : "Datastreams,Sensors,Things,Locations,HistoricalLocations,Observations,FeaturesOfInterest,MultiDatastreams".split(",")) {
+            testDiscoveryByPath(entity, "GET");
+            testDiscoveryByPath(entity, "HEAD");
+        }
+        for (String entity : rootTopicSingle) {
+            testDiscoveryByPath(entity, "GET");
+            testDiscoveryByPath(entity, "HEAD");
+        }
+        for (String entity : rootTopicsOthers) {
+            testDiscoveryByPath(entity, "GET");
+            testDiscoveryByPath(entity, "HEAD");
+        }
+
+    }
+
+    public void createFeatureOfInterest() throws IOException {
+        LOGGER.info("createFeatureOfInterest()");
+        String request = "{\"@iot.id\":1,\"name\":\"Munich\",\"description\":\"A nice place on Earth\",\"encodingType\":\"application/geo+json\",\"feature\":{\"type\":\"Point\",\"coordinates\":[11.509234,48.110728]}}";
+        HttpPost httpPost = new HttpPost(serverSettings.getServiceUrl(version) + "/FeaturesOfInterest");
+        HttpEntity stringEntity = new StringEntity(request, ContentType.APPLICATION_JSON);
+        httpPost.setEntity(stringEntity);
+
+        try (CloseableHttpResponse response = serviceSTAplus.execute(httpPost)) {
+
+            if (response.getStatusLine().getStatusCode() == 201) {
+                Assertions.assertTrue(true, "FeatureOfInterest created");
+            } else {
+                fail(response, "FeatureOfInterest creation failed");
+            }
+        }
+    }
+
+    public Map<String, String> getLinkHeaders(Header links[]) {
+        if (links == null)
+            return new HashMap<>(0);
+
+        Map<String, String> result = new HashMap<>(2);
+
+        for (Header link : links) {
+            for (HeaderElement element : link.getElements()) {
+                Matcher matcher = Pattern.compile("<(.*)>; .*rel=(.*)").matcher(element.toString());
+                if (matcher.find())
+                    result.put(matcher.group(2), matcher.group(1));
+
+            }
+        }
+        return result;
+    }
+
+    private void testDiscoveryByPath(String entity, String method) throws IOException {
+        LOGGER.info("  testDiscoveryByPath " + method + " (%s)".formatted(entity));
+
+        String url = serverSettings.getServiceUrl(version) + "/" + entity;
+
+        HttpRequestBase http = null;
+        if (method.equalsIgnoreCase("GET"))
+            http = new HttpGet(url.trim());
+        else
+            http = new HttpHead(url.trim());
+
+        try (CloseableHttpResponse response = serviceSTAplus.execute(http)) {
+            int expectedStatusCode = Integer.parseInt(TEST_DATA.get(entity)[0]);
+
+            Map<String, String> linkHeaders = getLinkHeaders(response.getHeaders("Link"));
+            String hubLink = linkHeaders.get("hub");
+            Assertions.assertTrue(hubLink != null, "hub not null");
+            Assertions.assertTrue(SERVER_PROPERTIES.get("plugins.stawebsub.hubUrl").equalsIgnoreCase(hubLink), "hub match");
+            String selfLink = linkHeaders.get("self");
+            String helpLink = linkHeaders.get("help");
+            Assertions.assertTrue(response.getStatusLine().getStatusCode() == expectedStatusCode, "response status code for method=" + method + " url=" + url + ": actual=" + response.getStatusLine().getStatusCode() + " expected=" + expectedStatusCode);
+            if (TEST_DATA.get(entity)[1] == null) {
+                Assertions.assertTrue(selfLink == null, "requested entityset is in denied entity => there is no Link rel=self");
+            } else {
+                String expectedSelfLink = serverSettings.getServiceUrl(version) + "/" + TEST_DATA.get(entity)[1];
+                Assertions.assertTrue(expectedSelfLink.equalsIgnoreCase(selfLink), "self-link match");
+            }
+            if (TEST_DATA.get(entity)[2] != null) {
+                // Testing help-Link
+                String expectedHelpLink = SERVER_PROPERTIES.get("plugins.stawebsub.helpUrl") + "#" + TEST_DATA.get(entity)[2];
+                Assertions.assertTrue(helpLink.equalsIgnoreCase(expectedHelpLink), "help-link match");
+            }
+        }
+    }
+
+    private void fail(CloseableHttpResponse response, String assertion) throws ParseException, IOException {
+        HttpEntity entity = response.getEntity();
+        String msg = "";
+        if (entity != null) {
+            msg = org.apache.http.util.EntityUtils.toString(entity);
+        }
+
+        Assertions.fail(assertion, new Throwable(msg));
+    }
+
+    public static class DiscoveryPathTestsAll extends DiscoveryPathTests {
+
+        static {
+            // Test configuration
+            SERVER_PROPERTIES.put("plugins.stawebsub.topicsDenied", "");
+            SERVER_PROPERTIES.put("plugins.multiDatastream.enable", "true");
+
+            // Core Data Model entities
+            TEST_DATA.put("Datastreams", new String[]{"200", "Datastreams", null});
+            TEST_DATA.put("Sensors", new String[]{"200", "Sensors", null});
+            TEST_DATA.put("Things", new String[]{"200", "Things", null});
+            TEST_DATA.put("Locations", new String[]{"200", "Locations", null});
+            TEST_DATA.put("HistoricalLocations", new String[]{"200", "HistoricalLocations", null});
+            TEST_DATA.put("Observations", new String[]{"200", "Observations", null});
+            TEST_DATA.put("FeaturesOfInterest", new String[]{"200", "FeaturesOfInterest", null});
+            // MultiDatastream
+            TEST_DATA.put("MultiDatastreams", new String[]{"200", "MultiDatastreams", null});
+            // Single Entity
+            TEST_DATA.put("FeaturesOfInterest(1)", new String[]{"200", "FeaturesOfInterest(1)", null});
+
+        }
+
+        public DiscoveryPathTestsAll() {
+            super(ServerVersion.v_1_1);
+        }
+    }
+
+    public static class DiscoveryPathTestMD0 extends DiscoveryPathTests {
+
+        static {
+            // Test configuration
+            SERVER_PROPERTIES.put("plugins.multiDatastream.enable", "false");
+            SERVER_PROPERTIES.put("plugins.stawebsub.topicsDenied", "v1.1/Datastreams,v1.1/Sensors,v1.1/Things,v1.1/Locations,v1.1/HistoricalLocations,v1.1/Observations,v1.1/FeaturesOfInterest,v1.1/Parties,v1.1/Licenses,v1.1/Campaigns,v1.1/ObservationGroups,v1.1/Relations,v1.1/FeaturesOfInterest(1)");
+
+            // Core Data Model entities
+            TEST_DATA.put("Datastreams", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            TEST_DATA.put("Sensors", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            TEST_DATA.put("Things", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            TEST_DATA.put("Locations", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            TEST_DATA.put("HistoricalLocations", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            TEST_DATA.put("Observations", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            TEST_DATA.put("FeaturesOfInterest", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            // MultiDatastream
+            TEST_DATA.put("MultiDatastreams", new String[]{"404", null, null});
+            // Single Entity
+            TEST_DATA.put("FeaturesOfInterest(1)", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+        }
+
+        public DiscoveryPathTestMD0() {
+            super(ServerVersion.v_1_1);
+        }
+    }
+
+    public static class DiscoveryPathTestMD1 extends DiscoveryPathTests {
+
+        static {
+            // Test configuration
+            SERVER_PROPERTIES.put("plugins.multiDatastream.enable", "true");
+            SERVER_PROPERTIES.put("plugins.stawebsub.topicsDenied", "v1.1/Datastreams,v1.1/Sensors,v1.1/Things,v1.1/Locations,v1.1/HistoricalLocations,v1.1/Observations,v1.1/FeaturesOfInterest,v1.1/Parties,v1.1/Licenses,v1.1/Campaigns,v1.1/ObservationGroups,v1.1/Relations,v1.1/FeaturesOfInterest(1)");
+
+            // Core Data Model entities
+            TEST_DATA.put("Datastreams", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            TEST_DATA.put("Sensors", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            TEST_DATA.put("Things", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            TEST_DATA.put("Locations", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            TEST_DATA.put("HistoricalLocations", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            TEST_DATA.put("Observations", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            TEST_DATA.put("FeaturesOfInterest", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+            // MultiDatastream
+            TEST_DATA.put("MultiDatastreams", new String[]{"200", "MultiDatastreams", null});
+            // Single Entity
+            TEST_DATA.put("FeaturesOfInterest(1)", new String[]{"200", null, TAG_ERROR_TOPIC_NOT_ALLOWED});
+        }
+
+        public DiscoveryPathTestMD1() {
+            super(ServerVersion.v_1_1);
+        }
+    }
+
+}
---
+++ src/test/java/de/securedimensions/frostserver/plugin/stawebsub/DiscoveryQueryTests.java
@@ -0,0 +1,262 @@
+/*
+ * Copyright (C) 2024 Secure Dimensions GmbH, D-81377
+ * Munich, Germany.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.securedimensions.frostserver.plugin.stawebsub.test;
+
+import static de.securedimensions.frostserver.plugin.stawebsub.PluginWebSub.TAG_ERROR_ODATA_EXPAND_DISABLED;
+import static de.securedimensions.frostserver.plugin.stawebsub.PluginWebSub.TAG_ERROR_ODATA_FILTER_DISABLED;
+
+import de.fraunhofer.iosb.ilt.frostclient.SensorThingsService;
+import de.fraunhofer.iosb.ilt.frostclient.models.SensorThingsPlus;
+import de.fraunhofer.iosb.ilt.frostclient.models.SensorThingsV11MultiDatastream;
+import de.fraunhofer.iosb.ilt.frostclient.models.SensorThingsV11Sensing;
+import de.fraunhofer.iosb.ilt.statests.AbstractTestClass;
+import de.fraunhofer.iosb.ilt.statests.ServerVersion;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Tests for the W3C Discovery for URLs that have a query segment (an ODATA part)
+ * e.g. http://localhost:8080/FROST-Server/v1.1/Observations?$select=result
+ *
+ * @author Andreas Matheus
+ */
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public abstract class DiscoveryQueryTests extends AbstractTestClass {
+
+    /**
+     * The logger for this class.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(DiscoveryQueryTests.class);
+    private static final long serialVersionUID = 1639739965;
+    private static final Map<String, String> SERVER_PROPERTIES = new LinkedHashMap<>();
+    static {
+        SERVER_PROPERTIES.put("mqtt.enabled", "false");
+        SERVER_PROPERTIES.put("plugins.plugins", "de.securedimensions.frostserver.plugin.stawebsub.PluginWebSub");
+        SERVER_PROPERTIES.put("plugins.stawebsub.enable", "true");
+        SERVER_PROPERTIES.put("plugins.stawebsub.hubUrl", "https://websub-hub.citiobs.secd.eu/api/subscriptions");
+        SERVER_PROPERTIES.put("plugins.stawebsub.helpUrl", "https://github.com/securedimensions/FROST-Server-WebSub/help");
+    }
+
+    private static Map<String, String[]> TEST_DATA = new HashMap<>();
+    static {
+        // ODATA commands that are not restricted
+        TEST_DATA.put("Observations?" + URLEncoder.encode("$top=1"), new String[]{"200", "Observations?$top=1", null, null});
+        TEST_DATA.put("Observations?" + URLEncoder.encode("$select=result"), new String[]{"200", "Observations?$select=result", null, null});
+        TEST_DATA.put("Observations?" + URLEncoder.encode("$select=result,phenomenonTime"), new String[]{"200", "Observations?$select=result%2CphenomenonTime", null, null});
+        TEST_DATA.put("Observations?" + URLEncoder.encode("$orderBy=phenomenonTime asc"), new String[]{"200", "Observations?$orderBy=phenomenonTime%20asc", null, null});
+        TEST_DATA.put("Observations?" + URLEncoder.encode("$select=result,phenomenonTime&$orderBy=phenomenonTime asc"), new String[]{"200", "Observations?$select=result%2CphenomenonTime&$orderBy=phenomenonTime%20asc", null, null});
+    }
+    protected static SensorThingsService serviceSTAplus;
+
+    public DiscoveryQueryTests(ServerVersion version) {
+        super(version, SERVER_PROPERTIES);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        LOGGER.info("Tearing down.");
+    }
+
+    @Override
+    protected void setUpVersion() {
+        LOGGER.info("Setting up for version {}.", version.urlPart);
+        serviceSTAplus = new SensorThingsService(
+                new SensorThingsV11Sensing(),
+                new SensorThingsV11MultiDatastream(),
+                new SensorThingsPlus());
+        try {
+            serviceSTAplus.setBaseUrl(new URL(serverSettings.getServiceUrl(version)));
+            serviceSTAplus.init();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*
+     * Success: self Link is not returned or equal to request URL
+     */
+    @Test
+    public void testDiscoveryWithQuery() throws IOException {
+        for (String key : TEST_DATA.keySet()) {
+            testDiscoveryWithQuery(key, "GET");
+            testDiscoveryWithQuery(key, "HEAD");
+        }
+    }
+
+    public Map<String, String> getLinkHeaders(Header links[]) {
+        if (links == null)
+            return new HashMap<>(0);
+
+        Map<String, String> result = new HashMap<>(2);
+
+        for (Header link : links) {
+            for (HeaderElement element : link.getElements()) {
+                Matcher matcher = Pattern.compile("<(.*)>; .*rel=(.*)").matcher(element.toString());
+                if (matcher.find())
+                    if (result.containsKey(matcher.group(2))) {
+                        String value = result.get(matcher.group(2));
+                        result.put(matcher.group(2), value + "," + matcher.group(1));
+                    } else
+                        result.put(matcher.group(2), matcher.group(1));
+            }
+        }
+        return result;
+    }
+
+    private void testDiscoveryWithQuery(String key, String method) throws IOException {
+        LOGGER.info("  testDiscoveryWithQuery " + method + " (%s)".formatted(key));
+
+        String url = serverSettings.getServiceUrl(version) + "/" + key;
+
+        HttpRequestBase http = null;
+        if (method.equalsIgnoreCase("GET"))
+            http = new HttpGet(url);
+        else
+            http = new HttpHead(url);
+
+        try (CloseableHttpResponse response = serviceSTAplus.execute(http)) {
+            int expectedStatusCode = Integer.parseInt(TEST_DATA.get(key)[0]);
+
+            Map<String, String> linkHeaders = getLinkHeaders(response.getHeaders("Link"));
+            String hubLink = linkHeaders.get("hub");
+            Assertions.assertTrue(hubLink != null, "hub not null");
+            Assertions.assertTrue(SERVER_PROPERTIES.get("plugins.stawebsub.hubUrl").equalsIgnoreCase(hubLink), "hub match");
+            String selfLink = linkHeaders.get("self");
+            String helpLink = linkHeaders.get("help");
+            Assertions.assertTrue(response.getStatusLine().getStatusCode() == expectedStatusCode, "response status code match");
+            if (TEST_DATA.get(key)[1] == null) {
+                // No self-link
+                Assertions.assertTrue(selfLink == null, "requested entityset is in deniedTopic => there is no Link rel=self");
+            } else {
+                // Testing self-link
+                String expectedSelfLink = URLDecoder.decode(url).replaceAll(",", "%2C").replaceAll(" ", "%20");
+                Assertions.assertTrue(expectedSelfLink.equalsIgnoreCase(selfLink), "self-link match");
+            }
+            if (TEST_DATA.get(key)[2] != null) {
+                String[] links = TEST_DATA.get(key)[2].split(",");
+                for (int ix = 0; ix < links.length; ix++) {
+                    links[ix] = SERVER_PROPERTIES.get("plugins.stawebsub.helpUrl") + "#" + links[ix];
+                }
+                for (String link : links)
+                    Assertions.assertTrue(Arrays.asList(links).contains(link), "help-link match");
+            }
+        }
+    }
+
+    private void fail(CloseableHttpResponse response, String assertion) throws ParseException, IOException {
+        HttpEntity entity = response.getEntity();
+        String msg = "";
+        if (entity != null) {
+            msg = org.apache.http.util.EntityUtils.toString(entity);
+        }
+
+        Assertions.fail(assertion, new Throwable(msg));
+    }
+
+    public static class DiscoveryWithQuery00 extends DiscoveryQueryTests {
+
+        static {
+            // Test configuration
+            SERVER_PROPERTIES.put("plugins.stawebsub.enable.odataQuery", "true");
+            SERVER_PROPERTIES.put("mqtt.allowFilter", "false");
+            SERVER_PROPERTIES.put("mqtt.allowExpand", "false");
+
+            TEST_DATA.put("Observations?" + URLEncoder.encode("$filter=result gt 30"), new String[]{"200", null, TAG_ERROR_ODATA_FILTER_DISABLED});
+            TEST_DATA.put("Observations?" + URLEncoder.encode("$expand=Datastream"), new String[]{"200", null, TAG_ERROR_ODATA_EXPAND_DISABLED});
+            TEST_DATA.put("Observations?" + URLEncoder.encode("$expand=Datastream&$filter=result gt 30"), new String[]{"200", null, TAG_ERROR_ODATA_EXPAND_DISABLED + "," + TAG_ERROR_ODATA_FILTER_DISABLED});
+        }
+
+        public DiscoveryWithQuery00() {
+            super(ServerVersion.v_1_1);
+        }
+    }
+
+    public static class DiscoveryWithQuery01 extends DiscoveryQueryTests {
+
+        static {
+            // Test configuration
+            SERVER_PROPERTIES.put("plugins.stawebsub.enable.odataQuery", "true");
+            SERVER_PROPERTIES.put("mqtt.allowFilter", "false");
+            SERVER_PROPERTIES.put("mqtt.allowExpand", "true");
+
+            TEST_DATA.put("Observations?" + URLEncoder.encode("$filter=result gt 30"), new String[]{"200", null, TAG_ERROR_ODATA_FILTER_DISABLED});
+            TEST_DATA.put("Observations?" + URLEncoder.encode("$expand=Datastream"), new String[]{"200", "Observations?$expand=Datastream", null, null});
+            TEST_DATA.put("Observations?" + URLEncoder.encode("$expand=Datastream&$filter=result gt 30"), new String[]{"200", null, TAG_ERROR_ODATA_FILTER_DISABLED});
+        }
+
+        public DiscoveryWithQuery01() {
+            super(ServerVersion.v_1_1);
+        }
+    }
+
+    public static class DiscoveryWithQuery10 extends DiscoveryQueryTests {
+
+        static {
+            // Test configuration
+            SERVER_PROPERTIES.put("plugins.stawebsub.enable.odataQuery", "true");
+            SERVER_PROPERTIES.put("mqtt.allowFilter", "true");
+            SERVER_PROPERTIES.put("mqtt.allowExpand", "false");
+
+            TEST_DATA.put("Observations?" + URLEncoder.encode("$filter=result gt 30"), new String[]{"200", "Observations?$filter=result%20gt%2030", null, null});
+            TEST_DATA.put("Observations?" + URLEncoder.encode("$expand=Datastream"), new String[]{"200", null, TAG_ERROR_ODATA_EXPAND_DISABLED});
+            TEST_DATA.put("Observations?" + URLEncoder.encode("$expand=Datastream&$filter=result gt 30"), new String[]{"200", null, TAG_ERROR_ODATA_EXPAND_DISABLED});
+        }
+
+        public DiscoveryWithQuery10() {
+            super(ServerVersion.v_1_1);
+        }
+    }
+
+    public static class DiscoveryWithQuery11 extends DiscoveryQueryTests {
+
+        static {
+            // Test configuration
+            SERVER_PROPERTIES.put("plugins.stawebsub.enable.odataQuery", "true");
+            SERVER_PROPERTIES.put("mqtt.allowFilter", "true");
+            SERVER_PROPERTIES.put("mqtt.allowExpand", "true");
+
+            TEST_DATA.put("Observations?" + URLEncoder.encode("$filter=result gt 30"), new String[]{"200", "Observations?$filter=result%20gt%2030", null, null});
+            TEST_DATA.put("Observations?" + URLEncoder.encode("$expand=Datastream"), new String[]{"200", "Observations?$expand=Datastream", null, null});
+            TEST_DATA.put("Observations?" + URLEncoder.encode("$expand=Datastream&$filter=result gt 30"), new String[]{"200", "$expand=Datastream&$filter=result gt 30", null});
+        }
+
+        public DiscoveryWithQuery11() {
+            super(ServerVersion.v_1_1);
+        }
+    }
+}
